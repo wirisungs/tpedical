13cf56e1ccb62ee5c9543fd69f9aa38a35515a83
Willis "Thiên Phúc" Crympt <phucplaymc@gmail.com>
2025-01-01 17:08:22 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/tpedical/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.tpedical
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:tpedical/app/app.dart';
+import 'package:tpedical/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
