7b1d9ff2d08e691dbcd7b9ed14f7bf61149ae06e
ThrisRH <144113163+ThrisRH@users.noreply.github.com>
2025-01-04 11:54:13 +0700

    basic function UI homepage

--- lib/app/view/main_views/home_page.dart
+++ lib/app/view/main_views/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:tpedical/components/colors.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -20,8 +22,15 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: const Center(
-          child: Text('CenterText'),
+        body: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SearchBarArea(),
+            SizedBox(
+              height: 16,
+            ),
+            ServiceArea(),
+          ],
         ),
         bottomNavigationBar: Stack(
           children: [
@@ -54,7 +63,6 @@ class _HomePageState extends State<HomePage> {
               unselectedFontSize: 10,
               showSelectedLabels: true,
               showUnselectedLabels: true,
-              // ignore: lines_longer_than_80_chars
               selectedLabelStyle: const TextStyle(fontWeight: FontWeight.w700),
             ),
             Positioned(
@@ -67,6 +75,164 @@ class _HomePageState extends State<HomePage> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class SearchBarArea extends StatefulWidget {
+  const SearchBarArea({super.key});
+
+  @override
+  State<SearchBarArea> createState() => _SearchBarAreaState();
+}
+
+class _SearchBarAreaState extends State<SearchBarArea> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 212,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/search_background.png'),
+          fit: BoxFit.cover,
+        ),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(12),
+          bottomRight: Radius.circular(12),
+        ),
+      ),
+      child: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 12),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                'MEDICAL APPOINTMENT BOOKING',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Text(
+                'Book Your Doctor Appointments Effortlessly',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                ),
+              ),
+              Text(
+                'Anytime, Anywhere',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter symptoms, hospital name, specialty,...',
+                  hintStyle: TextStyle(color: AppColor.normalGray),
+                  fillColor: Colors.white,
+                  filled: true,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                    borderSide: BorderSide(color: AppColor.primary),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                    borderSide: BorderSide(color: AppColor.primary),
+                  ),
+                ),
+                style: TextStyle(
+                  color: AppColor.normalGray,
+                  fontSize: 12,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ServiceArea extends StatelessWidget {
+  const ServiceArea({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'SERVICES',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w700,
+              color: AppColor.normalGray,
+            ),
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+          Row(
+            children: [
+              Column(
+                children: [
+                  IconButton(
+                    splashColor: Colors.transparent,
+                    highlightColor: Colors.transparent,
+                    icon: Image.asset(
+                      'assets/svg/hospital.png',
+                      width: 72,
+                    ),
+                    onPressed: () {},
+                  ),
+                  const SizedBox(
+                    width: 100,
+                    child: Text(
+                      'BOOKING HOSPITAL APPOINTMENT',
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(fontSize: 8, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  IconButton(
+                    splashColor: Colors.transparent,
+                    highlightColor: Colors.transparent,
+                    icon: Image.asset(
+                      'assets/svg/clinic.png',
+                      width: 72,
+                    ),
+                    onPressed: () {},
+                  ),
+                  const SizedBox(
+                    width: 100,
+                    child: Text(
+                      'BOOKING CLINIC APPOINTMENT',
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(fontSize: 8, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
---
+++ lib/components/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primary = Color(0xFF0065C0);
+  // Text colors
+  static const Color lightGray = Color(0xFFD3D3D3);
+  static const Color normalGray = Color(0xFF808080);
+  static const Color mateGray = Color(0xFF28282B);
+}
